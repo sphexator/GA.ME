dc2672779bc010dc50b583600ce1d50eeb16a47f
Kristoffer T <kristoffer.tunlid@gmail.com>
2026-03-02 22:28:37 +0100

    Integrate Disqord bot hosting with Discord API configuration

--- Game/Program.cs
+++ Game/Program.cs
@@ -1,8 +1,22 @@
+using Disqord;
+using Disqord.Bot.Hosting;
+using Disqord.Gateway;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
-var app = builder.Build();
+builder.Host.ConfigureDiscordBot((_, bot) =>
+{
+    bot.Token = builder.Configuration.GetValue<string>("Discord:Token");
+    bot.OwnerIds = [builder.Configuration.GetValue<ulong>("Discord:OwnerId")];
+    bot.ApplicationId = builder.Configuration.GetValue<ulong>("Discord:ApplicationId");
+    bot.UseMentionPrefix = true;
+    bot.Intents = GatewayIntents.All;
+    bot.Status = UserStatus.DoNotDisturb;
+    bot.Activities = [new LocalActivity("with the Grand Archive API", ActivityType.Playing)];
+});
 
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
